af6ca3ef681b67dabd2cc9c8521044bf82c5ecce
AbhijnaKalbhag <abhijnakalbhag@gmail.com>
2024-04-20 20:14:37 +0530

    new version of assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -1,35 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <limits.h>
-#include <bitset>
+#include<iostream>
+#include<math.h>
+#include <algorithm>
 using namespace std;
 
-int assignment(int cost[3][3], int n) {
-    vector<int> dp(1 << n, INT_MAX);
-    dp[0] = 0;
-    int x, y;
-    string mask;
-    int intmask;
-
-    for (int i = 0; i < (1 << n); i++) {
-        x = __builtin_popcount(i);
-        mask = bitset<10>(i).to_string().substr(10 - n); 
-        intmask = stoi(mask, 0, 2);
-
-        for (int j = 0; j < n; j++) {
-            if (mask[n - j - 1] != '1') {
-                y = (intmask | (1 << j));
-                dp[y] = min(dp[y], dp[intmask] + cost[x][j]);
-            }
+int N=3;
+int cost[3][3]={{3,2,7},
+                {5,1,3},
+                {2,7,2}};
+int count_set_bits(int N){
+    int count=0;
+    while(N>0){
+        if(N&1>0){
+            count++;
         }
+        N>>=1;
     }
-
-    return dp[(1 << n) - 1];
+    return count;
 }
 
-int main() {
-    int n = 3;
-    int cost[3][3] = {{3, 2, 7}, {5, 1, 3}, {2, 7, 2}};
-    cout << assignment(cost, n);
-    return 0;
+int main(){
+    int n=pow(2,N);
+    int dp[n];
+    for(int i=0;i<n;i++){
+        dp[i]=INT_MAX;
+    }
+    dp[0]=0;
+    int k;
+    for(int mask=0;mask<n;mask++){
+        int x=count_set_bits(mask);
+         k=mask;
+         k<<=1;
+        for(int j=0;j<N;j++){
+            if(!(k&1<<j)){
+            dp[mask | 1<<j]=min(dp[mask | 1<<j],dp[mask]+cost[x][j]);
+            }
+            k<<=1;
+    
+        }
+       
+    }
+
+    cout<<"Cost : "<<dp[n-1];
 }
